ac6ee7137cb943cae540c44cbe789c878e70d91f
Brian Joseph <joseph.b@husky.neu.edul>
2019-10-16 21:16:22 -0400

    models added

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class settings:
+    project_path = os.path.join(os.environ["CODE_PATH"], "oblak")
+    model_folder_path = os.path.join(project_path, "models")
